1f07d1a11232e3c83b4165242db3cacc48ed93e3
MagaraH <magarahesbon@gmail.com>
2023-03-16 13:22:26 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - its a void function
+ * uses puts function to print on the starndard output
+ *
+ *Description: The return value is 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
