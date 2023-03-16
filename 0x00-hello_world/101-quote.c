81ecc75aa64edb1e002538f25c53f6beea4334b3
MagaraH <magarahesbon@gmail.com>
2023-03-16 14:52:12 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - this program prints an a given comment
+ *
+ * Description: the main uses putchar to print on starndard output
+ * Return: The function has a return value 1
+ */
+int main(void)
+{
+	putchar("\"and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return(1)
+}
