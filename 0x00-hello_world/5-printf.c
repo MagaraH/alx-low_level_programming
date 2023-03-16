e12a01601a92e917816b1c7eba1e5e556ea40be2
MagaraH <magarahesbon@gmail.com>
2023-03-16 13:55:55 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main: uses printf fun to print on stdio
+ *
+ * Description: We are using printf
+ * Return: the return value will be 0 meaning we use int on main
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
