d84c9e3ac7dbbdbe95a8d6b8a36df66e87fc6ad1
MagaraH <magarahesbon@gmail.com>
2023-03-17 12:22:06 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - cintains code for this program
+ *
+ * Description: this code is a game
+ * it uses the function putchar to print alphabeths
+ * in lower case
+ * it use while loop to go through each lower case letter
+ * Return: (0)
+ */
+char main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+		;
+	}
+	putchar('\n');
+	return (0);
+}
+
