121e0c34d507bf9f113cad5c56c2c7568f4b3e8f
MagaraH <magarahesbon@gmail.com>
2023-03-21 12:07:35 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - function tha print last digit
+ * @digit: function parameter
+ * Return: lastDigit
+ */
+int print_last_digit(int digit)
+{
+	int lastDigit;
+
+	lastDigit = digit % 10;
+	if (digit < 0)
+		k = -k;
+	_putchar(k + '0');
+	return (k);
+}
