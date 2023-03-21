a59524a70da43d7d0fe0dc8405babc6cc876cac3
MagaraH <magarahesbon@gmail.com>
2023-03-21 12:12:03 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,7 @@ int print_last_digit(int digit)
 
 	lastDigit = digit % 10;
 	if (digit < 0)
-		k = -k;
-	_putchar(k + '0');
-	return (k);
+		lastDigit = -lastDigit;
+	_putchar(lastDigit + '0');
+	return (lastDigit);
 }
