7d84eaf33c8dbaa42cfc89c5a503a74d04693f6a
MagaraH <magarahesbon@gmail.com>
2023-03-21 18:25:40 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of 24hrs
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int hours;
+	int minutes;
+
+	if (hours = 0 ; hours < 24 ; hours++)
+	{
+		for (minutes = 0 ; minutes < 60 ; minutes++)
+		{
+			_putchar(hours / 10 + '0');/*prints first digit*/
+			_putchar(hours % 10 + '0');/*prints second hour digit*/
+			_putchar(':');
+			_putchar(minutes / 10 + '0');/*prints first letter for minutes*/
+			_putchar(minutes % 10 + '0');/*prints second letter for minutes*/
+			_putchar('\n');
+		}
+	}
+}
