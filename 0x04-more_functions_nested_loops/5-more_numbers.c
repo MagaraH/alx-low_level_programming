53c43f46a6c2838236c8ae62f9d5550cc85f0da1
MagaraH <magarahesbon@gmail.com>
2023-03-23 15:06:41 -0700

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,11 +13,10 @@ void more_numbers(void)
 	{
 		for (number = 0 ; number <= 14 ; number++)
 		{
-			if (number < 9)
-			{
-				_putchar((number / 10) + '0');
-				_putchar((number % 10) + '0');
-			}
+			if (number >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
 		}
+		_putchar('\n');
 	}
 }
