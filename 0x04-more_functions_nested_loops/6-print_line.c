f7e904a838cb242c4eb8e7b742f5f166ade9e90a
MagaraH <magarahesbon@gmail.com>
2023-03-23 13:05:12 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - prints a straight line
+ *
+ * @n: function parameter
+ * Return: 0
+ */
+void print_line(int n)
+{
+	char i;
+
+	if (i <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i > 0 ; i <= n ; i++)
+		{
+			_putchar('\n');
+		}
+		_putchar('\n')
+	}
+}
