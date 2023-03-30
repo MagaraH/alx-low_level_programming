53a67327584f6e083a428520c41f7ed8009d834a
MagaraH <magarahesbon@gmail.com>
2023-03-30 16:24:49 -0700

    Task 0

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,15 +9,19 @@ char *rot13(char *s)
 {
 	int i;
 
-	for (i = 0 ; s[i] != '\0' ; i++)
+		int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ((s[i] >= 'A' && s[i] <= 'M') || (s[i] >= 'a' && s[i] <= 'm'))
+		for (j = 0; j < 52; j++)
 		{
-			s[i] += 13;
-		}
-		else if ((s[i] >= 'N' && s[i] <= 'Z') || (s[i] >= 'n' && s[i] <= 'z'))
-		{
-			s[i] -= 13;
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
 		}
 	}
 	return (s);
