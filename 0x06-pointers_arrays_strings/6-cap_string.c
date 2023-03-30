185bf6bf1e318d213ab233bb736094d66fcb0db2
MagaraH <magarahesbon@gmail.com>
2023-03-30 06:01:35 -0700

    Task 6 correction

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * cap_string - function that capitalizes first letter of word
  *
@@ -35,14 +34,4 @@ char *cap_string(char *str)
 	}
 
 	return (str);
-}
-int main(void)
-{
-    char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-    char *ptr;
-
-    ptr = cap_string(str);
-    printf("%s", ptr);
-    printf("%s", str);
-    return (0);
 }
