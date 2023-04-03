d40757d7f8f08e67b789c4e6e769b811f42399fe
MagaraH <magarahesbon@gmail.com>
2023-04-03 13:17:30 -0700

    Task 0 betty correction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 /**
  * *_memset - function that fills memory with a constant byte.
  *
- * @*s: memory to be filled
+ * @s: memory to be filled
  * @b: constant byte
  * @n: function parameter
  * Return: s
@@ -11,7 +11,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	for(; n > 0 ; i++)
+	for (; n > 0 ; i++)
 	{
 		s[i] = b;
 		n--;
