e93c1522549b76b61005b59c097dfeb917cca218
MagaraH <magarahesbon@gmail.com>
2023-04-06 16:52:57 -0700

    Task 8

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -2,7 +2,10 @@
 #define MAIN_H
 #include <unistd.h>
 
-int _putchar(char c);
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
