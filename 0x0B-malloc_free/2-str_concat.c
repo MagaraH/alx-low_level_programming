52028e2a6e9cb0a336074cae69a9894955929208
MagaraH <magarahesbon@gmail.com>
2023-04-12 13:20:06 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,7 +24,7 @@ char *str_concat(char *s1, char *s2)
 		for (i = 0 ; s2[i] != '\0' ; i++)
 			len2++;
 	}
-	len == len1 + len2;
+	len = len1 + len2;
 	conct = (char *)malloc(sizeof(char) * (len + 1));
 	if (conct == NULL)
 	{
