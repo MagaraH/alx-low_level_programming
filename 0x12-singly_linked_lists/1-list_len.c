17e72734e70926041b433493ae887ddad4ee60fe
MagaraH <magarahesbon@gmail.com>
2023-04-27 06:09:40 -0700

    Task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * size_t list_len - function that returns the number
+ * of elements in a linked list_t list
+ *
+ * @h: pointer to the first node
+ * Return: count of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count;
+
+	for (count = 0 ; h != NULL ; h = h-> next, count++)
+	{
+		/* return count */
+	}
+	return (count);
+}
